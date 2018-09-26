2cd7642b85af27de487d0c720ac52bb82e710a60
Mike <mikescarpace@yahoo.com>
2018-09-26 19:16:09 -0400

    adding test class for chapter 1

---
+++ src/main/java/Chapter1/Declarations/AccessControl/Test.java
@@ -0,0 +1,14 @@
+package Chapter1.Declarations.AccessControl;
+
+public class Test {
+	
+	public static void main(String args[]){
+		for(int i = 0; i<3; );//or ;
+		;;;;;//needed second statement (conditional statement
+		//in for loop
+		int i = 6;
+		if(i>1);
+		else System.out.println();
+	}
+
+}
